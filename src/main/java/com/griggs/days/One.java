30ff7b1bce11e433d14e5f5c8a9c8ba9d4770999
Danielle Griggs <daniellegriggs121@gmail.com>
2024-12-07 20:15:39 -0500

    Created new class and interface to read in sample and problem inputs and Day One Class and Main

---
+++ src/main/java/com/griggs/days/One.java
@@ -0,0 +1,13 @@
+package src.main.java.com.griggs.days;
+
+import src.main.java.com.griggs.FileByDay;
+
+public class One extends FileByDay {
+    
+    @Override
+    public String getSampleInputFilePath() {
+        return "One.txt";
+    }
+    
+}
+
