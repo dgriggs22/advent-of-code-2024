7ce0a8b81569894d4daa52582fc254dec857071d
Danielle Griggs <daniellegriggs121@gmail.com>
2024-12-07 20:25:49 -0500

    Updated file paths

--- src/main/days/One.java
+++ src/main/days/One.java
@@ -1,6 +1,6 @@
-package src.main.java.com.griggs.days;
+package main.java.com.griggs.days;
 
-import src.main.java.com.griggs.FileByDay;
+import main.java.com.griggs.FileByDay;
 
 public class One extends FileByDay {
     
